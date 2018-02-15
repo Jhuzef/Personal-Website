ad6a9af8ae1cd5dd6575de8e6ebcdde2986caa1e
Sybl <josephgonzales034@gmail.com>
2018-02-15 01:20:50 -0500

    E-mail Test

--- email.php
+++ email.php
@@ -7,9 +7,10 @@
   $email = $_POST['email'];
   $subject = $_POST['subject'];
   $comment = $_POST['message'] . "\nMessage sent by: " . $_POST['name'];
-  
+  echo $admin_email;
+  echo $email;
   //send email
-  mail($admin_email, "$subject", $comment, "From:" . $email);
+  mail($admin_email, $subject, $comment, "From:" . $email);
   
   //Email response
   echo "E-mail successful, I will get back to you as soon as possible.";
