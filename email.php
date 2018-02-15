443b608d6e21b0f54869d522c41d4a65fe1e5182
Sybl <josephgonzales034@gmail.com>
2018-02-15 01:37:58 -0500

    Adjusted E-mail

--- email.php
+++ email.php
@@ -1,22 +1,49 @@
 <?php
-//if "email" variable is filled out, send email
-  if (isset($_POST['email']))  {
-  
-  //Email information
-  $admin_email = "cryptoknights@mailinator.com";
-  $email = $_POST['email'];
-  $subject = $_POST['subject'];
-  $comment = $_POST['message'] . "\nMessage sent by: " . $_POST['name'] . "at the email: " . $email;
-  echo $admin_email;
-  echo $email;
-  //send email
-  mail($admin_email, $subject, $comment);
-  
-  //Email response
-  echo "E-mail successful, I will get back to you as soon as possible.";
+$to = "josephgonzales034@gmail.com";  // Your email address will be here
+if (!isset($_POST[name]) || empty($_POST[name]))
+{
+echo "You didn't fill in the NAME field!";
+exit;
+}
+else
+{
+$name = $_POST[name];
+}
+$subject = $_POST[subject];
+ if (empty($_POST["email"])) {
+    echo "You didn't fill in the FROM field!";
+    exit;
+  } else {
+    // check if e-mail address is well-formed
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      echo "You filled Invalid FROM field!";
+      exit;
+    }
+    else
+    {
+    $email = $_POST[email];
+    }
   }
-else {
-	echo "E-mail failed.";
+if (!isset($_POST[message]) || empty($_POST[message]))
+{
+echo "You didn't fill in the MESSAGE field!";
+exit;
+}
+else
+{
+$message = $_POST[message];
+}
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=iso-8859-1" . "\r\n"; 
+$headers .= "From: " . $email . " <" . $email . ">" . "\r\n";
+if (mail($to, $subject, $message, $headers))
+{
+echo"<p>Email Sent !!!</p>\n";
+}
+else
+{
+echo"<p>Error!</p>\n";
+echo"<p>The mail() function failed.</p>";
 }
 
 ?>
