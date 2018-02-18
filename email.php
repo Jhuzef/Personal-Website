d218005dfe421645f107a15608fec42e5ef60818
Sybl <josephgonzales034@gmail.com>
2018-02-18 13:32:30 -0500

    Complete the Contact Form

--- email.php
+++ email.php
@@ -3,16 +3,23 @@
 $to = "josephgonzales034@gmail.com";  // Your email address will be here
 if (!isset($_POST[name]) || empty($_POST[name]))
 {
-echo "You didn't fill in the NAME field!";
+echo "You didn't fill in the name field!";
 exit;
 }
 else
 {
 $name = $_POST[name];
 }
-$subject = $_POST[subject];
+ if (empty($_POST["subject"])) {
+    echo "You didn't fill in the subject field!";
+    exit;
+  } else {
+ 
+    $subject = $_POST[subject];
+  }
+
  if (empty($_POST["email"])) {
-    echo "You didn't fill in the FROM field!";
+    echo "You didn't fill in the e-mail field!";
     exit;
   } else {
     // check if e-mail address is well-formed
@@ -20,7 +27,7 @@ $subject = $_POST[subject];
   }
 if (!isset($_POST[message]) || empty($_POST[message]))
 {
-echo "You didn't fill in the MESSAGE field!";
+echo "You didn't fill in the message field!";
 exit;
 }
 else
@@ -32,12 +39,12 @@ $headers .= "Content-type:text/html;charset=iso-8859-1" . "\r\n";
 $headers .= "From: " . $email . " <" . $email . ">" . "\r\n";
 if (mail($to, $subject, $message, $headers))
 {
-echo"<p>Email Sent</p>\n";
+echo"E-mail Sent";
 }
 else
 {
-echo"<p>Error!</p>\n";
-echo"<p>The mail() function failed.</p>";
+echo"E-mail failed. Heroku does not allow the use of the php mailer function. E-mail me directly, and I apologize for the inconvenience";
+
 } 
 
 ?>
